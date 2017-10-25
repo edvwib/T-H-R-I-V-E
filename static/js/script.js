0ae9649cac4026165fa1999790a133529cc41586
Edvin Wiberg <edde275@gmail.com>
2017-10-25 22:52:26 +0200

    change when nav changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $(document).on("scroll",function(){
-    if($(document).scrollTop()>250){
+    if($(document).scrollTop()>175){
       $("nav").removeClass("large").addClass("small");
     } else{
       $("nav").removeClass("small").addClass("large");

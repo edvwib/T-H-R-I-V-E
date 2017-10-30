79baff442e62fd56a4f691b2629a5e24a6ac5776
edvwib <edde275@gmail.com>
2017-10-30 22:34:50 +0100

    disable nav animation

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-  $(document).on("scroll",function(){
-    if($(document).scrollTop()>175){
-      $("nav").removeClass("large").addClass("small");
-    } else{
-      $("nav").removeClass("small").addClass("large");
-    }
-  });
+  // $(document).on("scroll",function(){
+  //   if($(document).scrollTop()>175){
+  //     $("nav").removeClass("large").addClass("small");
+  //   } else{
+  //     $("nav").removeClass("small").addClass("large");
+  //   }
+  // });
 
 
 

46cfebef73fcf91d67c09b92eca5cf0f0a331e42
edvwib <edde275@gmail.com>
2017-11-18 16:25:08 +0100

    improvements to mobile popups

--- static/js/script.js
+++ static/js/script.js
@@ -14,6 +14,30 @@ $(document).ready(function(){
   $('.cocoen').cocoen();
 
 
+
+  //Disable scrolling on popup mobile
+  // const overlays = document.querySelectorAll('.popup');
+  // overlays.forEach(function(overlay){
+  //   overlay.addEventListener('touchmove', function (e) {      
+  //     e.preventDefault();
+  //   }, false);
+  //   overlay.addEventListener('mousewheel', function (e) {
+  //     e.preventDefault();
+  //   }, false);
+  // });
+    
+  window.addEventListener('hashchange', function(){
+    url = window.location.href.split("#").pop();
+    console.log(url);
+    if (url === 'popup2'){
+      console.log('hej');
+      
+    }
+  });
+  
+
+  
+
   //Triangle hover
   //Top
   // $('#triangleTop').click(function(){
